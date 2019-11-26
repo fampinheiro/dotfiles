eb5c7039f96963f41084f8b3a0b333349a4083e6
Filipe Pinheiro <me@fampinheiro.com>
2019-11-26 23:23:31 +0000

    chore: export nvm env vars

--- source/50_node.sh
+++ source/50_node.sh
@@ -5,3 +5,5 @@ if [[ "$(type -P brew)" ]]; then
 fi
 
 [[ "$1" != init && ! -e $NVM_DIR ]] && return 1
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
