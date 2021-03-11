65f857271a2452ec03f4a2c7ef2073243451f8d5
Filipe Pinheiro <dev@fampinheiro.com>
2021-03-11 19:59:44 +0000

    feat: install fisher to manage fish plugins

--- init/50_osx_system.sh
+++ init/50_osx_system.sh
@@ -1,4 +1,10 @@
 is_osx || return 1
+# https://github.com/mathiasbynens/dotfiles/blob/main/.macos
+
+
+# require password immediately after sleep or screen saver begins
+defaults write com.apple.screensaver askForPassword -int 1
+defaults write com.apple.screensaver askForPasswordDelay -int 0
 
 # avoid creating dsstore files on network volumes
 defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
@@ -12,17 +18,17 @@ defaults write com.apple.dock mineffect -string "scale"
 defaults write com.apple.dock orientation left
 defaults write com.apple.dock autohide -bool true
 defaults write com.apple.dock showhidden -bool true
+defaults write com.apple.dock autohide-delay -float 0
 
 # don't rearrange spaces based on most recent use
 defaults write com.apple.dock mru-spaces -bool false
 
-# don't show Dashboard as a space
+# don't show dashboard as a space
 defaults write com.apple.dock dashboard-in-overlay -bool true 
 
-killall Dock
-
-# disable Dashboard
-defaults write com.apple.dashboard mcx-disabled -bool true
+# keyboard: set a blazingly fast keyboard repeat rate
+defaults write NSGlobalDomain KeyRepeat -int 1
+defaults write NSGlobalDomain InitialKeyRepeat -int 10
 
 # trackpad: enable tap to click for this user and for the login screen
 defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
@@ -36,12 +42,22 @@ defaults -currentHost write NSGlobalDomain com.apple.trackpad.trackpadCornerClic
 defaults -currentHost write NSGlobalDomain com.apple.trackpad.enableSecondaryClick -bool true
 
 # trackpad: enable 3 fingers drag
+defaults write com.apple.dock scroll-to-open -bool true
 defaults -currentHost write NSGlobalDomain com.apple.trackpad.threeFingerSwipeGesture -int 1
+
+# trackpad: expose
+defaults write com.apple.dock mcx-expose-disabled -bool false
+defaults write com.apple.dock expose-animation-duration -float 0.1
+defaults write com.apple.dashboard mcx-disabled -bool true
 
 # only use UTF-8 in Terminal.app
 defaults write com.apple.terminal StringEncodings -array 4
 
 # don’t display the annoying prompt when quitting iTerm
 defaults write com.googlecode.iterm2 PromptOnQuit -bool false
-
 defaults write com.apple.finder AppleShowAllFiles -bool true
+
+# photos
+defaults -currentHost write com.apple.ImageCapture disableHotPlug -bool true
+
+killall Dock
